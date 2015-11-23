1d277ea8e4d534069549d24834cafc4c877a328f
Si Chen <sichen@Sis-MacBook-Pro.local>
2015-11-23 00:05:46 -0500

    Week 12: Adding iPad application with classifcation and segmentation code (and test and demo images).

---
+++ k-means/segmen.m
@@ -0,0 +1,31 @@
+close all;
+
+he = imread('segment.png');
+cform = makecform('srgb2lab');
+lab_he = applycform(he,cform);
+ab = double(lab_he(:,:,2:3));
+nrows = size(ab,1);
+ncols = size(ab,2);
+ab = reshape(ab,nrows*ncols,2);
+
+nColors = 3;
+% repeat the clustering 3 times to avoid local minima
+[cluster_idx, cluster_center] = kmeans(ab,nColors,'distance','sqEuclidean', ...
+                                      'Replicates',3);
+pixel_labels = reshape(cluster_idx,nrows,ncols);
+imshow(pixel_labels,[]), title('image labeled by cluster index');
+segmented_images = cell(1,6);
+rgb_label = repmat(pixel_labels,[1 1 3]);
+
+for k = 1:nColors
+    color = he;
+    color(rgb_label ~= k) = 0;
+    segmented_images{k} = color;
+end
+
+figure; imshow(segmented_images{1}), title('objects in cluster 1');
+figure; imshow(segmented_images{2}), title('objects in cluster 2');
+figure; imshow(segmented_images{3}), title('objects in cluster 3');
+% figure; imshow(segmented_images{4}), title('objects in cluster 4');
+% figure; imshow(segmented_images{5}), title('objects in cluster 5');
+% figure; imshow(segmented_images{6}), title('objects in cluster 6');
